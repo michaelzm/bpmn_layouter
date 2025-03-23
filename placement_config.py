b0472b033d8d8e323fbfff5c95c9bfed40dee9db
michaelzm <michael.zemler@hotmail.com>
2025-03-23 15:32:20 +0100

    adjust spacing config

--- placement_config.py
+++ placement_config.py
@@ -1,12 +1,14 @@
 lookup_placement = {}
 #padding left defines the arrow length
-lookup_placement["initial-spacing-left"] = 150
+lookup_placement["initial-spacing-left"] = 300
 lookup_placement["initial-spacing-top"] = 150
 lookup_placement["spacing-left"] = 50
 #y line spacing defines the grid y axes spacing
 lookup_placement["y-line-spacing"] = 100
-lookup_placement["x-line-spacing"] = 75
+lookup_placement["x-line-spacing"] = 125
 lookup_placement["task"] = {"width": 100, "height":80}
+lookup_placement["userTask"] = {"width": 100, "height":80}
+lookup_placement["serviceTask"] = {"width": 100, "height":80}
 lookup_placement["startEvent"] = {"width": 36, "height":36}
 lookup_placement["endEvent"] = {"width": 36, "height":36}
 lookup_placement["exclusiveGateway"] = {"width": 50, "height":50}
