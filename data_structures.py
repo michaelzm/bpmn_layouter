ef47b1b20849506016b1b617d00829207dd24677
michaelzm <michael.zemler@hotmail.com>
2025-03-23 15:25:29 +0100

    added edge data structure

--- data_structures.py
+++ data_structures.py
@@ -1,31 +1,46 @@
 class Node:
-    def __init__(self,node_element):
-        self.id = node_element["id"]
-        self.data = node_element
+    def __init__(self,id, label):
+        self.id = id
+        self.label = label
         #depth controls the x axis
         self.depth = float("-inf")
         #shift controls the y axis
         self.shift = float("inf")
-        self.parents = []
-        self.child_nodes = []
+        self.incoming = []
+        self.outgoing = []
+        self.type = []
+
+    def add_incoming(self, incoming_element):
+        self.incoming.append(incoming_element)
+
+    def add_outgoing(self, outgoing_element):
+        self.outgoing.append(outgoing_element)
+
+    def set_type(self, type):
+        self.type = type
 
     def set_depth(self, new_depth):
         print("changed depth from ", self.depth, " to ", new_depth)
         self.depth = new_depth
 
-    def update_depth(self):
-        if self.parents == []:
-            self.depth = 0
-            return True
-        min_parent_depth = max([i.depth for i in self.parents])
-        self.depth = min_parent_depth + 1
-        return True
-
     def set_shift(self, new_shift):
-        #only change shift if the parent is closer to this nodes depth
-        #only allow to set shifts that are smaller than current
-        if new_shift < self.shift:
-            self.shift = new_shift  
-    
-    def add_parent(self, parent):
-        self.parents.append(parent)
+        print("changed shift from ", self.depth, " to ", new_shift)
+        self.shift = new_shift  
+
+class Edge:
+    def __init__(self,id, label):
+        self.id = id
+        self.label = label
+        self.incoming = []
+        self.outgoing = []
+        self.direction = None
+        self.type = "edge"
+
+    def set_incoming(self, incoming_elements):
+        self.incoming = [incoming_elements]
+
+    def set_outgoing(self, outgoing_elements):
+        self.outgoing = [outgoing_elements]
+
+    def set_direction(self, direction):
+        self.direction = direction
